410a94f5e9b4294141661b3827bfa457933c3737
guabao <wjz270025872@gmail.com>
2017-01-06 14:01:48 -0500

    support function for CVXOPT
    version 0.1

---
+++ CODES/_cvxopt_support_function.py
@@ -0,0 +1,26 @@
+__doc__ = r'''
+CVXOPT support functions.
+Author: Jiazhou.
+Platform: python 2.7 with numpy, scipy, and cvxopt
+Version: V0.1
+Date: 2017-01-06
+'''
+
+# python module
+import datetime, time
+
+# numpy, scipy related
+import numpy
+import scipy
+import scipy.linalg
+
+# CVXOPT
+from cvxopt import solvers, matrix
+
+def _support_Px_LTCM():
+    raise
+    return None
+
+def _support_Px_LQTCM():
+    raise
+    return None
